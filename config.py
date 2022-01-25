91d651a6a8a8cdbbc856dff0d5f667bdce2f5fdb
Palkit Lohia <57593495+spookbite@users.noreply.github.com>
2022-01-26 04:48:23 +0530

    added stuff

---
+++ config.py
@@ -0,0 +1,18 @@
+##OPEN API STUFF
+OPENAI_API_KEY = 'OPENAI_API_KEY'
+
+
+## FLASK STUFF
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = 'SECRET_KEY'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
